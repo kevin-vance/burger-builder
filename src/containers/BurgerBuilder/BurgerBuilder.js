eeb346c6f01378dce391083d440636ad6d37c13d
Kevin Vance <hello@kevinvance.com>
2017-12-28 13:29:13 -0700

    add BuildControl and BuildControls

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,34 @@
 import React, {Component} from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            ingredients: {
+                salad: 0, 
+                bacon: 0, 
+                cheese: 0, 
+                meat: 0
+            }
+        }
+    }
+    // could do that, but I don't wanna
+    //state = {
+    //    ingredients: {
+    //        salad: 0, 
+    //        bacon: 0, 
+    //        cheese: 2, 
+    //        meat: 0
+    //    }
+    //}
     render() {
         return(
             <Aux>
-                <Burger />
-                Build Controls
+                <Burger ingredients={this.state.ingredients} />
+                <BuildControls/>
             </Aux>
         );
     }
